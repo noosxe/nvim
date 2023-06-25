49086a452940071c3f3080bb2d07ba759d8fce2a
Levon Kirakosyan <noosx.e@gmail.com>
2023-06-24 20:48:24 -0700

    Add telescope.

--- init.lua
+++ init.lua
@@ -28,10 +28,13 @@ vim.cmd[[colorscheme tokyonight-night]]
 
 vim.o.mouse = 'a'
 
--- LSP {{{
+-- Telescope {{{
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-
--- }}} LSP
-
+-- }}} Telescope
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -119,4 +119,11 @@ return require('packer').startup(function(use)
         end
     }
 
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'}
+    }
+}
+
 end)

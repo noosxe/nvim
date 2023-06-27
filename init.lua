8cbc29d250cf8bab86959ecb080a5ddfac9dfe29
Levon Kirakosyan <noosx.e@gmail.com>
2023-06-27 16:57:19 -0700

    Configure autocomplete to be fuzzy.

--- init.lua
+++ init.lua
@@ -9,33 +9,46 @@ vim.cmd [[ let g:svelte_preprocessors = ['typescript'] ]]
 -- GENERAL SETTINGS
 
 -- Spaces & Tabs {{{
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+
 -- }}} Spaces & Tabs
 
 -- UI Config {{{
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.showcmd = true
 vim.o.showmatch = true
 vim.o.cursorline = true
 vim.o.termguicolors = true
+vim.o.winblend = 30
 vim.diagnostic.config({
   virtual_text = true,
 })
 vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
+
 -- }}} UI Config
 
 -- Colors {{{
+
 vim.cmd[[colorscheme tokyonight-night]]
 vim.cmd[[highlight IndentBlanklineIndent1 guibg=#1a1b26 gui=nocombine]]
 vim.cmd[[highlight IndentBlanklineIndent2 guibg=#16161e gui=nocombine]]
+
 -- }}} Colors
 
+-- Behavior {{{
+
 vim.o.mouse = 'a'
+vim.o.wildignorecase = true
+vim.o.wildoptions = 'fuzzy,pum,tagfile'
+
+-- }}} Behavior
 
 -- Telescope {{{
 

dabe1c33a7c4e7795bb6194715bcff5f9ffcb729
Levon Kirakosyan <noosx.e@gmail.com>
2023-09-03 13:18:05 -0700

    Add nvim-treesitter plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,22 @@ vim.api.nvim_exec(
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function ()
+            require'nvim-treesitter.configs'.setup {
+                highlight = {
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
+    }
     use 'famiu/bufdelete.nvim'
     use {
         "nvim-neo-tree/neo-tree.nvim",
